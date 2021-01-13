34320429c1e7164dc03841e356a88f6e08e22508
zhangqian <zhangqianbrandy2012@gmail.com>
2021-01-13 09:41:54 +0800

    v1.0.4

---
+++ app/src/main/java/com/zhangqian/xyzandroidtool/CloseIoUtils.kt
@@ -0,0 +1,25 @@
+package com.zhangqian.testleancloud.XYZFunction
+
+import java.io.Closeable
+import java.io.IOException
+
+object CloseIoUtils {
+
+//    /**
+//     * 关闭IO
+//     * @param closeables closeables
+//     */
+//    fun closeIO(vararg closeables: Closeable) {
+//        if (closeables == null) return
+//        closeables
+//            .filterNotNull()
+//            .forEach {
+//                try {
+//                    it!!.close()
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//    }
+
+}
---
+++ app/src/main/java/com/zhangqian/xyzandroidtool/XYZAnFunc_kingfisher.kt
@@ -0,0 +1,43 @@
+package com.zhangqian.testleancloud.XYZFunction.XYZAnFunc
+
+
+//图片加载类似Swift 的kingfisher
+
+
+//To get an image imperatively, use the get suspend function:
+//
+//val drawable = Coil.get("https://www.example.com/image.jpg")
+
+//coil-kt /coil   Image loading for Android backed by Kotlin Coroutines. https://coil-kt.github.io/coil/
+
+
+// URL
+//imageView.load("https://www.example.com/image.jpg")
+//
+//// Resource
+//imageView.load(R.drawable.image)
+//
+//// File
+//imageView.load(File("/path/to/image.jpg"))
+
+// And more...
+//
+
+// Requests can be configured with an optional trailing lambda:
+//
+//imageView.load("https://www.example.com/image.jpg") {
+//    crossfade(true)
+//    placeholder(R.drawable.image)
+//    transformations(CircleCropTransformation())
+//}
+
+
+
+//https://github.com/cesarferreira/faker
+
+//https://github.com/cuongpm/youtube-dl-android
+
+//https://github.com/Ramotion
+
+//https://github.com/KotlinBy/awesome-kotlin#android-frameworks
+
