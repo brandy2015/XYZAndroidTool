fef23691d21cc66aeb024780defe354096010b0d
XYZ <10484324+brandy2015@users.noreply.github.com>
2021-10-20 22:39:14 +0800

    1.2.3
    1.2.3

--- app/src/main/java/com/zhangqian/xyzandroidtool/XYZFunctions.kt
+++ app/src/main/java/com/zhangqian/xyzandroidtool/XYZFunctions.kt
@@ -13,10 +13,11 @@ import com.google.android.material.internal.ContextUtils
 class XYZFunctions {
 }
 
+@JvmName("PrintX1")
 fun String.PrintX(){
     Log.d("已运行,输出控制台",this) }
 
-fun PrintXx(Message:String){
+fun PrintX(Message:String){
     Log.d("已运行,输出控制台",Message) }
 
 fun PrintX(Message:Int){
